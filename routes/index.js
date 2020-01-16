2191c51fa5e0abffae5422ed311645ff7506d2e6
kuantumleap123 <k3v_w1l@yahoo.com>
2020-01-17 01:31:00 +0700

    comment controller

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router()
 const user = require('./user')
 const posts = require('./posts')
+const comments = require('./comments')
 const authentication = require('../middlewares/auth')
 
 router.use('/user', user)
 
 router.use('/', authentication)
 router.use('/posts', posts)
+router.use('/comments', comments)
 
 module.exports = router
