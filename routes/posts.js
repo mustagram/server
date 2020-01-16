c88a83c4e6269ffd533cd43bccc065e611e194a0
kuantumleap123 <k3v_w1l@yahoo.com>
2020-01-16 16:44:14 +0700

    routes for /posts

---
+++ routes/posts.js
@@ -0,0 +1,36 @@
+const postsRouter = require("express").Router();
+const express = require("express");
+postsRouter.use(express.urlencoded({extended:true}));
+
+
+postsRouter.get('/',(req,res) => {
+    res.send('Post list');
+    //PostController.showAllPosts(req,res);
+});
+
+postsRouter.get('/:id',authorisation,(req,res) => {
+    res.send('Post detail');
+    //PostController.showPostById(req,res);
+});
+
+postsRouter.post('/',(req,res) => {
+    res.send('add Post');
+    //PostController.addPost(req,res);
+});
+
+postsRouter.put('/:id',authorisation,(req,res) => {
+    res.send('update Post');
+    //PostController.updatePost(req,res);
+});
+
+postsRouter.patch('/:id',authorisation,(req,res) => {
+    res.send('update Post');
+    //PostController.updatePost(req,res);
+});
+
+postsRouter.delete('/:id',authorisation,(req,res) => {
+    res.send('delete Post');
+    //PostController.deletePost(req,res);
+});
+
+module.exports = postsRouter;
