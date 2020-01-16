eb0cf8993f78b29dec7c8920016b1475dcc82531
kuantumleap123 <k3v_w1l@yahoo.com>
2020-01-16 20:16:59 +0700

    post controller

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const user = require('./user')
+const posts = require('./posts')
 
 router.use('/user', user)
+router.use('/posts', posts)
 
 module.exports = router
